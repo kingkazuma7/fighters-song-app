5fe65ce912327497b366e433abef9c87c100891c
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-03 11:29:58 +0900

    json 表示まで

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 格闘家の入場曲まとめサービス
+
+- cssフレームワーク:chakra-ui
+- フロント:React
+- バックエンド:json dummy
+- node:v16.13.1
+- react:^18.2.0
+- react-dom:^18.2.0
+- 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import fightersData from './json/fighters.json';
 
-function App() {
+function FightersList() {
+  const [fighters, setFighters] = useState([]);
+
+  useEffect(() => {
+    setFighters(fightersData.fighters);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
+      {fighters.map(fighter => (
+        <div key={fighter.id}>
+          <h2>{fighter.name}</h2>
+          <p>階級: {fighter.weight_class}</p>
+          <p>入場曲: {fighter.entrance_song}</p>
+          <p>所属団体: {fighter.organization}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default FightersList;
