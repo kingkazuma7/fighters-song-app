cbab784e2b0e94ba2c3d6f6987d6a83b1469d602
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-04 12:31:06 +0900

    head title追加

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from "@chakra-ui/react";
 import FightersList from './components//FightersList';
@@ -6,10 +6,14 @@ import FighterPage from './components/FighterPage';
 import Header from './components/Header/Header';
 
 function App() {
+  useEffect(() => {
+    document.title = "格闘家の入場曲まとめ";
+  }, []);
+  
   return (
     <ChakraProvider>
     <Router>
-      <Header />
+      <Header title="格闘家の入場曲まとめ" />
       <Routes>
         <Route path="/" element={<FightersList />} />
         <Route path="/fighters/:id" element={<FighterPage />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Box, Text } from "@chakra-ui/react";
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <Box bg="blue.500" color="white" p="6" shadow="lg" _hover={{ boxShadow: "xl" }} transition="0.3s">
-      <Text fontSize="xl" fontWeight="bold">格闘家の入場曲まとめ</Text>
+      <Text fontSize="xl" fontWeight="bold">{title}</Text>
     </Box>
   );
 }
