8b954553f034e2e4b18f5062423dfb87d014cb45
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-05 11:37:04 +0900

    heade click top

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Box, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({ title }) => {
+  const navigate = useNavigate();
+  
+  const scrollToTop = () => {
+    navigate("/");
+  };
+  
   return (
-    <Box bg="blue.500" color="white" p="6" shadow="lg" _hover={{ boxShadow: "xl" }} transition="0.3s">
+    <Box bg="blue.500" color="white" p="6" shadow="lg" _hover={{ boxShadow: "xl" }} transition="0.3s" onClick={scrollToTop}>
       <Text fontSize="xl" fontWeight="bold">{title}</Text>
     </Box>
   );
