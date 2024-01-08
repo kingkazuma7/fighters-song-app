ff37f932f628449530f2d458c60a9894556e497d
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-08 11:59:41 +0900

    コントローラー追加

--- backend-api/routes/web.php
+++ backend-api/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FighterController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('fighters', FighterController::class);
