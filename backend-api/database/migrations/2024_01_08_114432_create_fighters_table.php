5ae3c0a32619242eded43c53cdcddc62f415ecb3
kingkazuma7 <your.nasionasio7@gmail.com>
2024-01-08 11:48:11 +0900

    テーブルのスキーマを定義

--- backend-api/database/migrations/2024_01_08_114432_create_fighters_table.php
+++ backend-api/database/migrations/2024_01_08_114432_create_fighters_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('fighters', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('weight_class');
+            $table->string('entrance_song');
+            $table->json('organization'); // Storing an array as JSON
+            $table->string('youtube_link');
+            $table->string('image');
+            $table->string('image_source');
+            $table->date('birthday');
+            $table->integer('height');
             $table->timestamps();
         });
     }
